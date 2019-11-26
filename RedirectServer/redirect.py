deb9c0002a2c306219e807aa0e0dd4e092a2a583
Arthur Olga <arthur_olga@hotmail.com>
2019-11-26 16:25:09 -0300

    (fix) redirect

--- RedirectServer/redirect.py
+++ RedirectServer/redirect.py
@@ -3,15 +3,13 @@
 """Alternative version of the ToDo RESTful server implemented using the
 Flask-RESTful extension."""
 
-from flask import Flask, jsonify, abort, make_response
+from flask import Flask, jsonify, abort, make_response, request
 from flask_restful import Api, Resource, reqparse, fields, marshal
 from flask_httpauth import HTTPBasicAuth
 import requests
 import json
 import datetime
 import os
-
-from flask import Flask, redirect
 
 
 app = Flask(__name__)
@@ -23,15 +21,27 @@ password = "olga"
 @app.route('/')
 def root():
     print(new_url)
-    return redirect(new_url, code=302)
+    return requests.get(new_url, code=307).content
 
 
-@app.route('/<path:page>', methods=['GET', 'POST', 'PUT', 'DELETE'])
-def anypage(page):
-    to_url = '{new_url}/{page}'.format(page=page, new_url=new_url)
-    print(to_url)
-    return redirect(to_url,
-                    code=307)
+@app.route('/<path:page>', methods=['GET'])
+def getpage(page):
+    # to_url = '{new_url}/{page}'.format(page=page, new_url=new_url)
+    # print(to_url)
+    # return redirect(to_url,
+    #                 code=307)
+    r = requests.get(new_url+{page})
+    return r.content
+
+
+@app.route('/<path:page>', methods=['POST'])
+def postpage(page):
+    # to_url = '{new_url}/{page}'.format(page=page, new_url=new_url)
+    # print(to_url)
+    # return redirect(to_url,
+    #                 code=307)
+    r = requests.post('{new_url}/{page}', json=request.get_json())
+    return r.content
 
 
 if __name__ == '__main__':
